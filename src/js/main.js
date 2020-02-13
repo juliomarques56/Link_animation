eb4627064bd18ca88297ac0b1409a26e7915b6a2
unknown <jotamarques56@gmail.com>
2020-02-13 18:31:36 -0400

    layout reposnivo

--- src/js/main.js
+++ src/js/main.js
@@ -1,17 +1,15 @@
-$('.container').bind("mouseenter",function() {	
+$('.principal').bind("mouseenter",function() {	
     $(this).children(this).children('img.icon-img').fadeIn(200);
-    $(this).children(this).children('h5').css('color','#FFF');
     $(this).children('.coluna_logo').fadeOut(200);
-    $(this).children('.coluna_icon').animate( {"top":"40"},600,"easeOutBack");
+    $(this).children('.coluna_icon').animate( {"top":"10%"},600,"easeOutBack");
     $(this).children('.coluna_cliqueAqui').fadeIn(1);
-    $(this).children('.coluna_cliqueAqui').animate( {"top":"200"},600,"easeOutBack");
+    $(this).children('.coluna_cliqueAqui').animate( {"top":"65%"},600,"easeOutBack");
 });
 
-$('.container').bind("mouseleave",function(){
+$('.principal').bind("mouseleave",function(){
     $(this).children('.coluna_cliqueAqui').fadeOut(1);
-    $(this).children('.coluna_cliqueAqui').animate({"top":"280"},600,"easeOutBack");
+    $(this).children('.coluna_cliqueAqui').animate({"top":"90%"},600,"easeOutBack");
     $(this).children(this).children('img.icon-img').fadeOut(200);
-    $(this).children(this).children('h5').css('color','#333');
-    $(this).children('.coluna_icon').animate( {"top":"200"},600,"easeOutBack");
+    $(this).children('.coluna_icon').animate( {"top":"70%"},600,"easeOutBack");
     $(this).children('.coluna_logo').fadeIn(400);
 })
